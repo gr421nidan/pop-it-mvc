5c7f31e2400babd1879cd5e2d7a603a8361733e1
gr421nidan <nikushkina.dasha@yandex.ru>
2024-04-11 02:42:10 +0700

    Вурстка главных страницы авторизации

--- views/site/login.php
+++ views/site/login.php
@@ -1,13 +1,15 @@
-<h2>Авторизация</h2>
-<h3><?= $message ?? ''; ?></h3>
-
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
-    ?>
-    <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
-    </form>
-<?php endif;
+<div class="login_content">
+    <div class="login_content_block">
+        <h2>Войдите в свою учетную запись</h2>
+        <?php
+        if (!app()->auth::check()):
+            ?>
+            <form method="post" class="form_login">
+                <input class="field_login" type="text" name="login" placeholder="Логин"></label>
+                <input class="field_login" type="password" name="password" placeholder="Пароль"></label>
+                <button class="button_login">Войти</button>
+            </form>
+        <?php endif;
+        ?>
+    </div>
+</div>
