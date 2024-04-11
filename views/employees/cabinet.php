115a4622294ab17c0b6a1df4c5dfd68eac0ef991
gr421nidan <nikushkina.dasha@yandex.ru>
2024-04-11 19:31:38 +0700

    Исправила стиль у страницы личного кабинета

--- views/employees/cabinet.php
+++ views/employees/cabinet.php
@@ -6,7 +6,7 @@
                 <p class="text_info">Логин: <?= app()->auth::user()->login ?></p>
                 <p class="text_info">Пароль:<?= app()->auth::user()->password ?></p>
             </div>
-            <img src="/pop-it-mvc/public/image/cabinet.jpg" class="image" alt="Изображение">
+            <img src="/pop-it-mvc/public/image/cabinet.jpg" class="image_cabinet" alt="Изображение">
         </div>
     </div>
 </div>
